dbb39254bcc3a350a9e1aa1b9fecb5133f6461b7
jankincai <jankincai12@gmail.com>
2023-09-14 09:16:44 +0800

    Bump version: 0.4.1 → 0.4.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,8 @@ author = 'JanKinCai'
 
 # The full version, including alpha/beta/rc tags
 
-version = "0.4.1"
-release = "0.4.1"
+version = "0.4.2"
+release = "0.4.2"
 
 
 # -- General configuration ---------------------------------------------------
